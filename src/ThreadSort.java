89d6db9d2f65f857c2f8cba82d9b9cde957fdb6c
esquitx <diegods2065@gmail.com>
2023-09-14 19:34:23 +0200

    finish thread sort implementation

--- src/ThreadSort.java
+++ src/ThreadSort.java
@@ -42,51 +42,64 @@ public class ThreadSort {
         }
 
         for (int i = 0; i < arr.length; i += maxLim) {
-            int pivot = i == 0 ? 0 : i - 1;
+            int mid = i == 0 ? 0 : i - 1;
             int remain = (arr.length - i);
             int end = remain < maxLim ? i + (remain - 1) : i + (maxLim - 1);
 
+            merge(arr, 0, mid, end);
         }
 
     }
 
-    private static void exchange(int[] arr, int i, int j) {
+    public static void merge(int[] arr, int start, int mid, int end) {
+        int[] temp = new int[(end - start) + 1];
 
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        // Initialize swapping indexes
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
 
-    }
-
-    private static int partition(int[] arr, int start, int end) {
-
-        // Select pivot
-        int pivot = arr[end];
-
-        // Correct starting position
-        int k = (start - 1);
-
-        for (int i = start; i <= end - 1; i++) {
-
-            if (arr[i] < pivot) {
-
-                k++;
-                exchange(arr, k, i);
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                i += 1;
+            } else {
+                temp[k] = arr[j];
+                j += 1;
             }
+            k += 1;
         }
-        exchange(arr, k + 1, end);
-        return (k + 1);
+
+        // Add remaining elements to temp array from first half that are left over
+        while (i <= mid) {
+            temp[k] = arr[i];
+            i += 1;
+            k += 1;
+        }
+
+        // Add remaining elements to temp array from second half that are left over
+        while (j <= end) {
+            temp[k] = arr[j];
+            j += 1;
+            k += 1;
+        }
+
+        for (i = start, k = 0; i <= end; i++, k++) {
+            arr[i] = temp[k];
+        }
     }
 
-    // Based on QuickSort chapter of Algortihms - Fourth Edition - Sedgewick & Wayne
-    public static void quickSort(int[] arr, int start, int end) {
-
+    // Based on MergeSort chapter of Algorithms - Fourth Edition - Sedgewick & Wayne
+    void mergeSort(int[] arr, int start, int end) {
         if (start < end) {
-
-            int pi = partition(arr, start, end);
-            quickSort(arr, start, pi - 1);
-            quickSort(arr, pi + 1, end);
+            int mid = (start + end) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, start, mid, end);
         }
+    }
 
+    public int getThreads() {
+        return 0;
     }
 }
