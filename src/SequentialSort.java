8bccda9c77d1217368065198edcf9c9a9728688a
esquitx <diegods2065@gmail.com>
2023-09-16 18:24:02 +0200

    change location of merge aux function

--- src/SequentialSort.java
+++ src/SequentialSort.java
@@ -1,7 +1,5 @@
 class SequentialSort implements Sorter {
 
-    Auxiliary aux = new Auxiliary();
-
     // Implement MergeSort
     @Override
     public void sort(int[] arr) {
@@ -14,13 +12,53 @@ class SequentialSort implements Sorter {
 
     }
 
+    public static void merge(int[] arr, int start, int mid, int end) {
+        int[] temp = new int[(end - start) + 1];
+
+        // Initialize swapping indexes
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+
+        //
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                i += 1;
+            } else {
+                temp[k] = arr[j];
+                j += 1;
+            }
+            k += 1;
+        }
+
+        // Add "forgotten" elements from first
+        while (i <= mid) {
+            temp[k] = arr[i];
+            i += 1;
+            k += 1;
+        }
+
+        // Add "forgotten" elements to temp array from second half
+        while (j <= end) {
+            temp[k] = arr[j];
+            j += 1;
+            k += 1;
+        }
+
+        // Copy from temp to orginal array
+        for (i = start, k = 0; i <= end; i++, k++) {
+            arr[i] = temp[k];
+        }
+    }
+
     // Based on MergeSort chapter of Algorithms - Fourth Edition - Sedgewick & Wayne
     void mergeSort(int[] arr, int start, int end) {
         if (start < end) {
             int mid = (start + end) / 2;
             mergeSort(arr, start, mid);
             mergeSort(arr, mid + 1, end);
-            aux.merge(arr, start, mid, end);
+            merge(arr, start, mid, end);
         }
     }
 
